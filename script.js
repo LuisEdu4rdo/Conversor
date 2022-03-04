9f8d4004bdd448f79f4fbfdaf0dc171852b8910e
Luís Eduardo <luiseduardorrg@gmail.com>
2022-03-04 19:53:02 -0300

    preparando para receber novas entradas

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let celsius = document.getElementById("celsius");
 let resultado = document.getElementById("resultado");
+// let kelvin = document.getElementById("kelvin");
 
 function calcular() {
   let calc = (celsius.value * 9 / 5) + 32;
 
   resultado.innerHTML = `${celsius.value}°C é igual a ${calc}° F`;
+
 }

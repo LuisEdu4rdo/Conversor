070e391e696c964a9c02d833cf8eccdc01d2f3b4
Luís Eduardo <luiseduardorrg@gmail.com>
2022-03-05 04:01:57 -0300

    adicionando kelvin

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let celsius = document.getElementById("celsius");
 let resultado = document.getElementById("resultado");
-// let kelvin = document.getElementById("kelvin");
+let temp_select = document.getElementById("temp_select");
 
 function calcular() {
-  let calc = (celsius.value * 9 / 5) + 32;
-
-  resultado.innerHTML = `${celsius.value}°C é igual a ${calc}° F`;
-
+  if (temp_select.value == "fahrenheit") {
+    var calc = (celsius.value * 9 / 5) + 32;
+    resultado.innerHTML = `${celsius.value}°C é igual a ${calc}° F`;
+  }
+  else {
+    if (celsius.value == "") {
+      resultado.innerHTML = `${celsius.value}°C é igual a 273,15° K`;
+    }
+    else {
+      var calc = (parseInt(celsius.value) + parseFloat(273.15));
+      resultado.innerHTML = `${celsius.value}°C é igual a ${calc}° K`;
+    }
+  }
 }
